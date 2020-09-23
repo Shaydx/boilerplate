35f784ce758e79037a87917a0d06cfde477b47f8
Mishari Adel Toama <mat@npstoday.com>
2020-09-23 13:49:26 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,21 @@
+# boilerplate
+Browser-Sync, SASS, Boostrap and Gulp Boilerplate
+
+## To Run the Project
+After you clone this project you may try to install all dependencies using 
+
+```
+$ npm install
+```
+
+Before you start the server, you need to install gulp 4 globally.
+
+```
+$ npm install gulp -g
+```
+
+Then run the command below.
+
+```
+$ gulp watch
+```
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//compile scss into css
+function style() {
+    return gulp.src('src/assets/scss/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('src/assets/css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./src",
+           index: "/index.html"
+        }
+    });
+    gulp.watch('src/assets/scss/**/*.scss', style)
+    gulp.watch('./src/*.html').on('change',browserSync.reload);
+    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
